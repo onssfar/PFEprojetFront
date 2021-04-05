27e1c28442d8d07d9cda2559cdaad16d28fd757f
onssfar <60037038+onssfar@users.noreply.github.com>
2021-04-05 23:02:36 +0100

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { Page404Component } from './page404/page404.component';
+
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'**',
+    component:Page404Component
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
